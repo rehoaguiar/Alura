48b29bab12c959903ac607298aec46c025de48da
Reh <142178373+rehoaguiar@users.noreply.github.com>
2025-02-10 17:04:12 -0300

    Primeiro Curso de Python <3

---
+++ Python - Crie a sua primeira aplicação/restaurante.py
@@ -0,0 +1,19 @@
+# Apresentação do nome do restaurante
+print('''
+███████╗░█████╗░░██████╗████████╗  ██████╗░██╗███████╗████████╗
+██╔════╝██╔══██╗██╔════╝╚══██╔══╝  ██╔══██╗██║██╔════╝╚══██╔══╝
+█████╗░░███████║╚█████╗░░░░██║░░░  ██║░░██║██║█████╗░░░░░██║░░░
+██╔══╝░░██╔══██║░╚═══██╗░░░██║░░░  ██║░░██║██║██╔══╝░░░░░██║░░░
+██║░░░░░██║░░██║██████╔╝░░░██║░░░  ██████╔╝██║███████╗░░░██║░░░
+╚═╝░░░░░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░  ╚═════╝░╚═╝╚══════╝░░░╚═╝░░░
+''')
+
+# Opções do aplicativo
+print('1. Cadastrar Restaurante')
+print('2. Listar Restaurante')
+print('3. Ativar Restaurante')
+print('4. Sair')
+
+# Definindo variáveos
+opcao_escolhida = input('Escolha uma opção: ')
+print('Você escolheu a opção', opcao_escolhida)
