d8cc729cebbce193064bfb2bc4e2c507f16e42ad
Renata.Aguiar <renata.aguiar@telebras.com>
2025-02-12 10:29:31 -0300

    Função Cadastrar Restaurante + Tratamento de Erros

--- Python - Crie a sua primeira aplicação/restaurante.py
+++ Python - Crie a sua primeira aplicação/restaurante.py
@@ -2,21 +2,27 @@
 import os
 import time
 
+# Criando uma lista de restaurantes
+restaurantes = []
+
 # Definindo Funções
 # Apresentação do nome do restaurante
-def exibir_titulo():
+def exibir_nome_app():
     print('''
     ███████╗░█████╗░░██████╗████████╗  ██████╗░██╗███████╗████████╗
     ██╔════╝██╔══██╗██╔════╝╚══██╔══╝  ██╔══██╗██║██╔════╝╚══██╔══╝
     █████╗░░███████║╚█████╗░░░░██║░░░  ██║░░██║██║█████╗░░░░░██║░░░
     ██╔══╝░░██╔══██║░╚═══██╗░░░██║░░░  ██║░░██║██║██╔══╝░░░░░██║░░░
     ██║░░░░░██║░░██║██████╔╝░░░██║░░░  ██████╔╝██║███████╗░░░██║░░░
-    ╚═╝░░░░░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░  ╚═════╝░╚═╝╚══════╝░░░╚═╝░░░
-    ''')
+    ╚═╝░░░░░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░  ╚═════╝░╚═╝╚══════╝░░░╚═╝░░░''')
+
+def limpar_tela():
+    os.system('cls')
+    # os.system('clear') # No Sistema MAC utilizamos o comando clear
 
 # Opções do aplicativo
 def exibir_menu():
-    print('1. Cadastrar Restaurante')
+    print('\n1. Cadastrar Restaurante')
     print('2. Listar Restaurante')
     print('3. Ativar Restaurante')
     print('4. Sair')
@@ -27,30 +33,51 @@ def finalizar_app():
         print(f'O aplicativo será finalizado em {ii} segundos\n', end='', flush=True)
         time.sleep(1)
 
-    os.system('cls')
-    # os.system('clear') # No Sistema MAC utilizamos o comando clear
+        limpar_tela()
+   
+def main():
+    limpar_tela()
+    exibir_nome_app()
+    exibir_menu()
+    escolher_opcao()
+
+def opcao_invalida():
+    print('\nOpção inválida')
+    input('Digite uma tecla para voltar ao menu principal: ')
+    main()
+
+def cadastrar_restaurante():
+    limpar_tela()
+    exibir_nome_app()
+
+    print('''
+    █▀▀ ▄▀█ █▀▄ ▄▀█ █▀ ▀█▀ █▀█ █▀█   █▀▄ █▀▀   █▀█ █▀▀ █▀ ▀█▀ ▄▀█ █░█ █▀█ ▄▀█ █▄░█ ▀█▀ █▀▀ █▀
+    █▄▄ █▀█ █▄▀ █▀█ ▄█ ░█░ █▀▄ █▄█   █▄▀ ██▄   █▀▄ ██▄ ▄█ ░█░ █▀█ █▄█ █▀▄ █▀█ █░▀█ ░█░ ██▄ ▄█''')    
+    
+    nome_do_restaurante = input('\n\nDigite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    input('\nDigite uma tecla para voltar ao menu principal: ')
+    main()
 
 def escolher_opcao():
     # Definindo variáveos
-    opcao_escolhida = int(input('\nEscolha uma opção: '))
+    try:
+        opcao_escolhida = int(input('\nEscolha uma opção: '))
 
-    if (opcao_escolhida == 1):
-        print('Cadastrar Restaurante')
-
-    elif (opcao_escolhida == 2):
-        print('Listar Restaurante')
-
-    elif (opcao_escolhida == 3):
-        print('Ativar Restaurante')
-
-    else:
-        finalizar_app()
-
-
-def main():
-    exibir_titulo()
-    exibir_menu()
-    escolher_opcao()
+        match(opcao_escolhida):
+            case 1:
+                cadastrar_restaurante()
+            case 2:
+                print('Listar Restaurante')
+            case 3: 
+                print('Ativar Restaurante')
+            case 4:
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()
 
 if __name__ == '__main__':
     main()
