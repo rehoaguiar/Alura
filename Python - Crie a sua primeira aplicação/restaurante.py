8cb82bec5a03369fdc78511df4dca6423f8a85f8
rehoaguiar <renata.palatinho@gmail.com>
2025-02-19 22:21:50 -0300

    Criando a função Alterar Status + Melhorando o código

--- Python - Crie a sua primeira aplicação/restaurante.py
+++ Python - Crie a sua primeira aplicação/restaurante.py
@@ -3,7 +3,11 @@ import os
 import time
 
 # Criando uma lista de restaurantes
-restaurantes = ['Le Touchè','Always']
+restaurantes = [
+    {'nome':'Le Touchè','categoria':'francesa','ativo': False},
+    {'nome':'Always','categoria':'brasileira','ativo': True},
+    {'nome':'Forever','categoria':'americana','ativo': True}
+]
 
 # Definindo Funções
 # Apresentação do nome do restaurante
@@ -29,12 +33,12 @@ def montar_cabecalho(texto):
 def exibir_menu():
     print('\n1. Cadastrar Restaurante')
     print('2. Listar Restaurante')
-    print('3. Ativar Restaurante')
+    print('3. Alterar Status do Restaurante')
     print('4. Sair')
 
 def finalizar_app():
     
-    for ii in range(16,1):
+    for ii in range(15, 0, -1):
         print(f'O aplicativo será finalizado em {ii} segundos\n', end='', flush=True)
         time.sleep(1)
 
@@ -59,9 +63,14 @@ def cadastrar_restaurante():
     █▀▀ ▄▀█ █▀▄ ▄▀█ █▀ ▀█▀ █▀█ █▀█   █▀▄ █▀▀   █▀█ █▀▀ █▀ ▀█▀ ▄▀█ █░█ █▀█ ▄▀█ █▄░█ ▀█▀ █▀▀ █▀
     █▄▄ █▀█ █▄▀ █▀█ ▄█ ░█░ █▀▄ █▄█   █▄▀ ██▄   █▀▄ ██▄ ▄█ ░█░ █▀█ █▄█ █▀▄ █▀█ █░▀█ ░█░ ██▄ ▄█''')    
     
-    nome_do_restaurante = input('\n\nDigite o nome do restaurante que deseja cadastrar: ')
-    restaurantes.append(nome_do_restaurante)
-    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    nome_restaurante = input('\n\nDigite o nome do restaurante que deseja cadastrar: ')
+    categoria = input(f'Digite o nome da categoria do restaurante {nome_restaurante}: ')
+
+    dados_restaurante = {'nome':nome_restaurante, 'categoria':categoria, 'ativo':False}
+    restaurantes.append(dados_restaurante)
+
+    print(f'O restaurante {nome_restaurante} foi cadastrado com sucesso!')
+
     voltar_menu()
 
 def listar_restaurante():
@@ -69,9 +78,35 @@ def listar_restaurante():
     █░░ █ █▀ ▀█▀ ▄▀█   █▀▄ █▀▀   █▀█ █▀▀ █▀ ▀█▀ ▄▀█ █░█ █▀█ ▄▀█ █▄░█ ▀█▀ █▀▀ █▀
     █▄▄ █ ▄█ ░█░ █▀█   █▄▀ ██▄   █▀▄ ██▄ ▄█ ░█░ █▀█ █▄█ █▀▄ █▀█ █░▀█ ░█░ ██▄ ▄█\n''')
 
+    print(f'{"Nome do Restaurante".ljust(22)} | {"Categoria".ljust(20)} | Status')
     for restaurante in restaurantes:
-        print(f'. {restaurante}')
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = 'Ativado' if restaurante['ativo'] else 'Desativado'
+
+        print(f'- {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}')
     
+    voltar_menu()
+
+def alterar_status():
+    montar_cabecalho('''
+    ▄▀█ █░░ ▀█▀ █▀▀ █▀█ ▄▀█ █▀█   █▀ ▀█▀ ▄▀█ ▀█▀ █░█ █▀   █▀▄ █▀█   █▀█ █▀▀ █▀ ▀█▀ ▄▀█ █░█ █▀█ ▄▀█ █▄░█ ▀█▀ █▀▀
+    █▀█ █▄▄ ░█░ ██▄ █▀▄ █▀█ █▀▄   ▄█ ░█░ █▀█ ░█░ █▄█ ▄█   █▄▀ █▄█   █▀▄ ██▄ ▄█ ░█░ █▀█ █▄█ █▀▄ █▀█ █░▀█ ░█░ ██▄''')
+
+
+    nome_restaurante = input('\nDigite o nome do restaurante que deseja alterar o status: ').strip()
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome_restaurante.lower() == restaurante['nome'].lower():
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso!' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso!'
+            print(mensagem)
+            
+    if not restaurante_encontrado:
+        print(f'\nO restaurante {nome_restaurante} não foi encontrado')
+        
     voltar_menu()
 
 def escolher_opcao():
@@ -85,7 +120,7 @@ def escolher_opcao():
             case 2:
                 listar_restaurante()
             case 3: 
-                print('Ativar Restaurante')
+                alterar_status()
             case 4:
                 finalizar_app()
             case _:
