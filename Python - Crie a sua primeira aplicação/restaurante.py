7c69d79b7e4e6faea6a8746453b6493ef5d8f0b0
rehoaguiar <renata.palatinho@gmail.com>
2025-02-24 21:33:53 -0300

    Documentando as funções

--- Python - Crie a sua primeira aplicação/restaurante.py
+++ Python - Crie a sua primeira aplicação/restaurante.py
@@ -12,6 +12,8 @@ restaurantes = [
 # Definindo Funções
 # Apresentação do nome do restaurante
 def exibir_nome_app():
+    ''' Exibe o nome do programa, a fonte foi encontrada no site fsymbols '''
+    
     print('''
     ███████╗░█████╗░░██████╗████████╗  ██████╗░██╗███████╗████████╗
     ██╔════╝██╔══██╗██╔════╝╚══██╔══╝  ██╔══██╗██║██╔════╝╚══██╔══╝
@@ -25,19 +27,24 @@ def limpar_tela():
     # os.system('clear') # No Sistema MAC utilizamos o comando clear
 
 def montar_cabecalho(texto):
+    ''' Limpa a tela, exibe o nome do aplicativo e, por fim, recebendo o input texto, ele exibe o texto recebido na tela '''
+    
     limpar_tela()
     exibir_nome_app()
     print(texto)
 
 # Opções do aplicativo
 def exibir_menu():
+    ''' Mostra todas as 4 opções do menu, sendo elas: Cadastrar Restaurante; Listar Restaurantes; Alterar Status do Restaurante; Sair do aplicativo '''
+    
     print('\n1. Cadastrar Restaurante')
     print('2. Listar Restaurante')
     print('3. Alterar Status do Restaurante')
     print('4. Sair')
 
 def finalizar_app():
-    
+    ''' Inicia uma contagem regressiva de 15 segundos e limpa o prompt de comando, encerrando, então, o aplicativo '''
+
     for ii in range(15, 0, -1):
         print(f'O aplicativo será finalizado em {ii} segundos\n', end='', flush=True)
         time.sleep(1)
@@ -55,10 +62,14 @@ def voltar_menu():
     main()
 
 def opcao_invalida():
+    ''' Exibe opção inválida na tela e chama a função voltar ao menu '''
+    
     print('\nOpção inválida')
     voltar_menu()
 
 def cadastrar_restaurante():
+    ''' Exibe o nome da funcionalidade na tela e recebe o input do nome do restaurante a ser cadastrado. Em seguida, recebe também o nome da categoria e cria um dicionário com essas informações. Por fim, ele confirma ao usuário que o restaurante em questão foi cadastrado e volta para o menu '''
+    
     montar_cabecalho('''
     █▀▀ ▄▀█ █▀▄ ▄▀█ █▀ ▀█▀ █▀█ █▀█   █▀▄ █▀▀   █▀█ █▀▀ █▀ ▀█▀ ▄▀█ █░█ █▀█ ▄▀█ █▄░█ ▀█▀ █▀▀ █▀
     █▄▄ █▀█ █▄▀ █▀█ ▄█ ░█░ █▀▄ █▄█   █▄▀ ██▄   █▀▄ ██▄ ▄█ ░█░ █▀█ █▄█ █▀▄ █▀█ █░▀█ ░█░ ██▄ ▄█''')    
@@ -74,6 +85,8 @@ def cadastrar_restaurante():
     voltar_menu()
 
 def listar_restaurante():
+    ''' Exibe o nome da funcionalidade na tela e exibe o subtítulo (Nome do restaurante; Categoria; Status) seguido das informações dos restaurantes contidas em cada um deles. A função utiliza a estrutura de repetição for para listar cada um dos restaurantes '''
+    
     montar_cabecalho('''
     █░░ █ █▀ ▀█▀ ▄▀█   █▀▄ █▀▀   █▀█ █▀▀ █▀ ▀█▀ ▄▀█ █░█ █▀█ ▄▀█ █▄░█ ▀█▀ █▀▀ █▀
     █▄▄ █ ▄█ ░█░ █▀█   █▄▀ ██▄   █▀▄ ██▄ ▄█ ░█░ █▀█ █▄█ █▀▄ █▀█ █░▀█ ░█░ ██▄ ▄█\n''')
@@ -89,6 +102,8 @@ def listar_restaurante():
     voltar_menu()
 
 def alterar_status():
+    ''' Exibe o nome da funcionalidade na tela e recebe um imput com o nome do restaurante. Ele utiliza de uma estrutura de repetição for para ativar e desativar o restaurante digitado pelo usuário. Caso ele não tenha sido encontrado ele apresentará uma mensagem de erro '''
+    
     montar_cabecalho('''
     ▄▀█ █░░ ▀█▀ █▀▀ █▀█ ▄▀█ █▀█   █▀ ▀█▀ ▄▀█ ▀█▀ █░█ █▀   █▀▄ █▀█   █▀█ █▀▀ █▀ ▀█▀ ▄▀█ █░█ █▀█ ▄▀█ █▄░█ ▀█▀ █▀▀
     █▀█ █▄▄ ░█░ ██▄ █▀▄ █▀█ █▀▄   ▄█ ░█░ █▀█ ░█░ █▄█ ▄█   █▄▀ █▄█   █▀▄ ██▄ ▄█ ░█░ █▀█ █▄█ █▀▄ █▀█ █░▀█ ░█░ ██▄''')
@@ -110,7 +125,7 @@ def alterar_status():
     voltar_menu()
 
 def escolher_opcao():
-    # Definindo variáveos
+    ''' Ele pede para o usuário digitar o número da funcionalidade que ele deseja utilizar no código. Em caso de erro ele sinaliza que a opção é inválida '''
     try:
         opcao_escolhida = int(input('\nEscolha uma opção: '))
 
