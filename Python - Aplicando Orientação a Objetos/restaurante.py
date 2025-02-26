f4dc0621aee49abb64b3d00d506a588c91a79ec3
Renata.Aguiar <renata.aguiar@telebras.com>
2025-02-26 13:28:40 -0300

    Novo Curso Python <3

---
+++ Python - Aplicando Orientação a Objetos/restaurante.py
@@ -0,0 +1,12 @@
+# Criando a classe
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+# Criando dois novos restaurantes
+restaurante_praca = Restaurante()
+restaurante_pizza = Restaurante()
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+print(restaurantes)
