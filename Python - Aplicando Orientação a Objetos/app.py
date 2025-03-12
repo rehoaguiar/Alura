fe711d84d5038955380d202b332aa1a582243f84
rehoaguiar <renata.palatinho@gmail.com>
2025-03-11 22:36:30 -0300

    Pequenas alterações no código do projeto (Classe Prato e Classe Bebida)

--- Python - Aplicando Orientação a Objetos/app.py
+++ Python - Aplicando Orientação a Objetos/app.py
@@ -1,16 +1,14 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Place', 'Fast Food')
-
-restaurante_praca.receber_avaliacao('Reh', 3)
-restaurante_praca.receber_avaliacao('Teteu', 2)
-restaurante_praca.receber_avaliacao('May', 5)
-
-restaurante_praca.alternar_estado()
+restaurante_praca = Restaurante('praça', 'Gourmet')
+bebida_suco = Bebida ('Suco de Melancia', 5.0, 'grande')
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_paozinho)
 
 if __name__ == '__main__':
     main()
